e25fa02ef0ca7cfa67eb68671ca02b4ed1be45d8
MI-28-Havoc <mustafa-soput@gmx.de>
2020-12-17 09:22:55 +0100

    - Added author annotation

--- app/src/main/java/com/example/gruppe3_movieapp/AboutActivity.java
+++ app/src/main/java/com/example/gruppe3_movieapp/AboutActivity.java
@@ -6,11 +6,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import static com.example.gruppe3_movieapp.AppConstFunctions.applyBackgroundColor;
 
+/**
+ * @author Mustafa Soput
+ */
 public class AboutActivity  extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //ScrollView als oberstes Element übergeben...
         applyBackgroundColor(this, this, R.id.constrLayoutAbout);
     }
 
